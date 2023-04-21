2b9178f27d2e90390b8f6642216b532ca5a1c670
Eren <86615310+erent8@users.noreply.github.com>
2023-04-21 09:22:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+def replace_word():
+    str = "hi guys, i am tomi, and hi hi hi hi"
+    word_to_replace = input("Enter the word to replace: ")
+    word_replacement = input("Enter the word replacement: ")
+    print(str.replace(word_to_replace, word_replacement))
+    
+replace_word()
 
+# Eren Terzi
